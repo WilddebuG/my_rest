bccec3eb151df690b0836e6dc47186f1eafa62f3
Andriy Miga <andriy.miga@gmail.com>
2018-07-13 17:12:59 +0300

    fix to api/v1

--- frontend/config/_urlManager.php
+++ frontend/config/_urlManager.php
@@ -6,5 +6,6 @@ return [
     'showScriptName'=>false,
     'rules' => [
         ['class' => 'yii\rest\UrlRule', 'controller' => 'api/v1/default', 'only' => ['index']],
+        'GET api/v1'=>'api/v1/default',
     ],
 ];
