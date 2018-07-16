77f7dadd6e822e1a07013e7475a36326355cf1dc
Andriy Miga <andriy.miga@gmail.com>
2018-07-16 17:24:28 +0300

    add profile model and query

--- common/models/User.php
+++ common/models/User.php
@@ -6,20 +6,26 @@ use yii\base\NotSupportedException;
 use yii\behaviors\TimestampBehavior;
 use yii\db\ActiveRecord;
 use yii\web\IdentityInterface;
+use yii\behaviors\AttributeBehavior;
+use yii\helpers\ArrayHelper;
 
 /**
- * User model
+ * This is the model class for table "{{%user}}".
  *
- * @property integer $id
+ * @property int $id
  * @property string $username
+ * @property string $auth_key
+ * @property string $access_token
  * @property string $password_hash
  * @property string $password_reset_token
+ * @property string $oauth_client
+ * @property string $oauth_client_user_id
  * @property string $email
- * @property string $auth_key
- * @property integer $status
- * @property integer $created_at
- * @property integer $updated_at
- * @property string $password write-only password
+ * @property int $status
+ * @property int $created_at
+ * @property int $updated_at
+ *
+ * @property UserProfile $userProfile
  */
 class User extends ActiveRecord implements IdentityInterface
 {
@@ -36,12 +42,43 @@ class User extends ActiveRecord implements IdentityInterface
     }
 
     /**
+     * @return array
+     */
+    public function scenarios()
+    {
+        return ArrayHelper::merge(
+            parent::scenarios(),
+            [
+                'oauth_create' => [
+                    'oauth_client', 'oauth_client_user_id', 'email', 'username', '!status'
+                ]
+            ]
+        );
+    }
+
+    /**
      * {@inheritdoc}
      */
     public function behaviors()
     {
         return [
             TimestampBehavior::className(),
+            'auth_key' => [
+                'class' => AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'auth_key'
+                ],
+                'value' => Yii::$app->getSecurity()->generateRandomString()
+            ],
+            'access_token' => [
+                'class' => AttributeBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => 'access_token'
+                ],
+                'value' => function () {
+                    return Yii::$app->getSecurity()->generateRandomString(40);
+                }
+            ]
         ];
     }
 
@@ -51,8 +88,35 @@ class User extends ActiveRecord implements IdentityInterface
     public function rules()
     {
         return [
-            ['status', 'default', 'value' => self::STATUS_ACTIVE],
-            ['status', 'in', 'range' => [self::STATUS_ACTIVE, self::STATUS_DELETED]],
+            [['username', 'auth_key', 'access_token', 'password_hash', 'email', 'created_at', 'updated_at'], 'required'],
+            [['status', 'created_at', 'updated_at'], 'integer'],
+            [['username', 'password_hash', 'password_reset_token', 'oauth_client', 'oauth_client_user_id', 'email'], 'string', 'max' => 255],
+            [['auth_key'], 'string', 'max' => 32],
+            [['access_token'], 'string', 'max' => 40],
+            [['username'], 'unique'],
+            [['email'], 'unique'],
+            [['password_reset_token'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'username' => 'Username',
+            'auth_key' => 'Auth Key',
+            'access_token' => 'Access Token',
+            'password_hash' => 'Password Hash',
+            'password_reset_token' => 'Password Reset Token',
+            'oauth_client' => 'Oauth Client',
+            'oauth_client_user_id' => 'Oauth Client User ID',
+            'email' => 'Email',
+            'status' => 'Status',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
         ];
     }
 
@@ -69,7 +133,9 @@ class User extends ActiveRecord implements IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        throw new NotSupportedException('"findIdentityByAccessToken" is not implemented.');
+        return static::find()
+            ->andWhere(['access_token' => $token, 'status' => self::STATUS_ACTIVE])
+            ->one();
     }
 
     /**
@@ -81,6 +147,17 @@ class User extends ActiveRecord implements IdentityInterface
     public static function findByUsername($username)
     {
         return static::findOne(['username' => $username, 'status' => self::STATUS_ACTIVE]);
+    }
+
+    /**
+     * Finds user by username
+     *
+     * @param string $username
+     * @return static|null
+     */
+    public static function findByLogin($email)
+    {
+        return static::findOne(['email' => $email, 'status' => self::STATUS_ACTIVE]);
     }
 
     /**
@@ -186,4 +263,22 @@ class User extends ActiveRecord implements IdentityInterface
     {
         $this->password_reset_token = null;
     }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUserProfile()
+    {
+        return $this->hasOne(UserProfile::className(), ['user_id' => 'id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\User\UserQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\User\UserQuery(get_called_class());
+    }
+
 }
---
+++ common/models/UserProfile.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "{{%user_profile}}".
+ *
+ * @property int $user_id
+ * @property string $firstname
+ * @property string $middlename
+ * @property string $lastname
+ * @property string $avatar_path
+ * @property string $avatar_base_url
+ * @property string $locale
+ * @property int $gender
+ *
+ * @property User $user
+ */
+class UserProfile extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return '{{%user_profile}}';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['locale'], 'required'],
+            [['gender'], 'integer'],
+            [['firstname', 'middlename', 'lastname', 'avatar_path', 'avatar_base_url'], 'string', 'max' => 255],
+            [['locale'], 'string', 'max' => 32],
+            [['user_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['user_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'user_id' => 'User ID',
+            'firstname' => 'Firstname',
+            'middlename' => 'Middlename',
+            'lastname' => 'Lastname',
+            'avatar_path' => 'Avatar Path',
+            'avatar_base_url' => 'Avatar Base Url',
+            'locale' => 'Locale',
+            'gender' => 'Gender',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getUser()
+    {
+        return $this->hasOne(User::className(), ['id' => 'user_id']);
+    }
+
+    /**
+     * {@inheritdoc}
+     * @return \common\models\query\User\UserProfileQuery the active query used by this AR class.
+     */
+    public static function find()
+    {
+        return new \common\models\query\User\UserProfileQuery(get_called_class());
+    }
+}
